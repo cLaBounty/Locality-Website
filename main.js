a2b91489f97685ee1b503d2abe16d9eeba0070aa
cLaBounty <camlabounty@gmail.com>
2023-04-17 12:46:00 -0400

    Added Download Image Buttons

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-$(document).ready(() => {
-    // TODO: implement
-})
+const APP_STORE_URL = "https://www.google.com/"
+const GOOGLE_PLAY_URL = "https://www.google.com/"
+
+const onAppStoreDownloadClicked = () => {
+    window.open(APP_STORE_URL)
+}
+
+const onGooglePlayDownloadClicked = () => {
+    window.open(GOOGLE_PLAY_URL)
+}
