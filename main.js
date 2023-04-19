0ca90de793965d554af06756c3204c22592ce1a1
cLaBounty <camlabounty@gmail.com>
2023-04-19 02:41:26 -0400

    Updated Download Links

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const APP_STORE_URL = "https://www.google.com/"
-const GOOGLE_PLAY_URL = "https://www.google.com/"
+const APP_STORE_URL = "https://apps.apple.com/app/id6447739451"
+const GOOGLE_PLAY_URL = "https://play.google.com/store/apps/details?id=com.locality.locality.android"
 
 const onAppStoreDownloadClicked = () => {
     window.open(APP_STORE_URL)
